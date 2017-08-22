e58a91c95b49cfe186534dbfa3354e1f93f514b4
Alicia Good <agood@opentext.com>
2017-08-21 22:32:32 -0500

    Updated attack buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,20 +23,6 @@ var opponentCounterAttackPT = 0;
 //var lifeLeft = (baseHealthPoints - injuryPoints);
 var injuryPoints = 0;
 
-
-
-function resetGame () {
-		resetUI();  // 0. Start game and reset reset() from ui
-		gameAnswer = chooseWord();  // 1. chooseWord() from rules
-		console.log('this is the word: ' + gameAnswer);
-		var hintResult = words.indexOf(gameAnswer);
-		chooseHint(hintResult);
-		gameShownAnswer = blanksFromAnswer(gameAnswer);  // 2. show "_" blanksFromAnswer from rules
-		hangmanState = 0;
-		drawWord(gameShownAnswer);    // 3. initialize ui drawWord from ui
-		hideHints();
-}
-
 //$(document).ready(resetGame);
 var audioElement = document.createElement('audio');
 var imageElement = document.createElement('image');
@@ -56,7 +42,9 @@ function attack () {
 
 	if (yourHealth <= 0) {
 		myWins++;
+		yourLosses++;
 		drawMyWins(myWins);
+		drawYourLosses(yourLosses);
 		$('.msg-text').remove();
 		yourHealth = 0;
 		yourCounter = 0;
@@ -66,18 +54,42 @@ function attack () {
 		$('#msg-display').append(
 			$('<span/>').addClass('msg-text pull-right').text("Select a new opponent to continue"));
 		$('#vanquished-display').append (
-			$('<div/>').addClass('shown-picture pull-left').html(players[j].pix));
+			$('<div/>').addClass('shown-vanquished pull-left').html(players[j].pix));
 		
 	} else {
 		alert("Your opponent is still alive");
 		$('.shown-your-health').remove();
 		$('#your-health-display').append(
-					$('<span/>').addClass('shown-your-health').text(yourHealth));
+			$('<span/>').addClass('shown-your-health').text(yourHealth));
+		counterAttack();
+	}
+}
+
+function counterAttack () {
+	myHealth = (myHealth - yourCounter);
+	
+	if (myHealth <= 0) {
+		myLosses++;
+		yourWins++;
+		drawMyLosses(myLosses);
+		drawYourWins(yourWins);
+		$('.msg-text').remove();
+		myHealth = 0;
+		myAttack = 0;
+		alert("You are dead!");
+		resetPlayerStats();
+		$('#msg-display').append(
+			$('<span/>').addClass('msg-text pull-right').text("Game over, press 'reset game' to play again"));
+	} else {
+		alert("You have been hit!");
+		$('.shown-health').remove();
+		$('#health-display').append(
+			$('<span/>').addClass('shown-health').text(myHealth));
 	}
 }
 
 function resetOpponentStats() {
-	alert("You are resetting player stats now");
+	alert("You are resetting opponents stats now");
 	yourHealth = 0;
 	yourCounter = 0;
 	$('.opponent-display').remove();
@@ -87,6 +99,11 @@ function resetOpponentStats() {
 		$('<span/>').addClass('shown-your-health').text(yourHealth));
 	$('#counter-display').append(
 		$('<span/>').addClass('shown-counter').text(yourCounter));
+}
+
+function resetPlayerStats () {
+	alert("You are resetting game stats now");
+	resetUI();
 }
 
 function win () { 
@@ -102,38 +119,6 @@ function lose () {
 	resetGame(); 
 }
 
-function doKeypress () {
-    console.log("Entering the doKeypress function");
-	var tempChar = $('#letter-input').val().toLowerCase();
-	console.log("This is value of tempChar: " + tempChar);
-    var tempString = "";
-	$('#letter-input').val("");
-    
-    // Write here!
-    tempString = guessLetter( tempChar, gameShownAnswer, gameAnswer );
-	console.log("This is value of tempString:" + tempString);
-    if ( tempString != gameShownAnswer ) {
-        updateWord( tempString );
-        gameShownAnswer = tempString;
-        if ( gameShownAnswer === gameAnswer ) {
-            myWins = myWins + 1;
-/*			$( "#toggle" ).attr( "style", "visibility: visible;" ); */
-			dsplWinnerBoard();
-			drawMyWins(myWins);
-			win();
-        }
-    } else {
-        wrongLetter(tempChar);
-        drawSequence[ hangmanState++ ]();
-        if ( hangmanState === drawSequence.length ) {
-            myLosses = myLosses + 1;
-			drawMyLosses(myWins);
-			lose();
-        }
-    }  
-}
-$('#letter-input').keyup( doKeypress );
-
 function dsplWinnerBoard () {
 	$( "#toggle" ).attr( "style", "visibility: visible;" ); 
 	$("img").removeAttr("style");
@@ -144,13 +129,18 @@ function dsplWinnerBoard () {
 // --- BGN ui ---
 
 function resetUI () {
-    $('.body-part').remove();
-    $('.guessed-letter').remove();
-    $('.shown-letter').remove();
-	$('.shown-wins').remove();
-	$('.shown-losses').remove();
-	$('.shown-hint').remove();
-/*	$('.shown-picture').remove(); */
+	myWins = 0;
+	myLosses = 0;
+	myHealth = 0;
+	myAttack = 0;
+	yourWins = 0;
+	yourLosses = 0;
+	yourHealth = 0;
+	yourCounter = 0;
+	$('.shown-picture').remove();
+	$('.opponent-display').remove();
+	$('.shown-vanquished').remove();
+	$('.shown-health').remove();
 	drawMyWins(myWins);
 	drawMyLosses(myLosses);
 	drawMyHealth(myHealth);
@@ -161,68 +151,52 @@ function resetUI () {
 	drawYourCounter(yourCounter);
 	dsplWinnerBoard();
 }
-function drawWord( answer ) {
-    for ( i in answer ) {
-	$('.word-display').append(
-	    $('<span/>').addClass('shown-letter').html('&nbsp;'));
-    }
-}
-function drawHints( hintValue ) {
-    
-	if (hintValue != "") {
-		$('#hint-display').append(
-	    $('<span/>').addClass('shown-hint').text(hintValue));
-		console.log("This is the value for hintValue: " + hintValue);
-	};
 
-}
 function drawMyWins( myWins ) {
     $('.shown-wins').remove();
 	$('#wins-display').append(
 	    $('<span/>').addClass('shown-wins').text(myWins));
     
 }
-function drawYourWins( yourWins ) {
-    
-	$('#your-wins-display').append(
-	    $('<span/>').addClass('shown-your-wins').text(yourWins));
-    
-}
 function drawMyLosses( myLosses ) {
-    
+    $('.shown-losses').remove();
 	$('#losses-display').append(
 	    $('<span/>').addClass('shown-losses').text(myLosses));
     
 }
-function drawYourLosses( yourLosses ) {
+function drawMyHealth ( myHealth ) {
+	$('.shown-health').remove();
+	$('#health-display').append(
+		$('<span/>').addClass('shown-health').text(myHealth));
+}
+function drawMyAttack( myAttack ) {
+	$('.shown-attack').remove();
+	$('#attack-display').append(
+		$('<span/>').addClass('shown-attack').text(myAttack));
+}
+function drawYourWins( yourWins ) {
+    $('.shown-your-wins').remove();
+	$('#your-wins-display').append(
+	    $('<span/>').addClass('shown-your-wins').text(yourWins));
     
+}
+
+function drawYourLosses( yourLosses ) {
+    $('.shown-your-losses').remove();
 	$('#your-losses-display').append(
 	    $('<span/>').addClass('shown-your-losses').text(yourLosses));
     
 }
-function drawMyHealth( myHealth ) {
-    
-	$('#health-display').append(
-	    $('<span/>').addClass('shown-health').text(myHealth));
-    
-}
-function drawMyAttack( myAttack ) {
-    
-	$('#attack-display').append(
-	    $('<span/>').addClass('shown-attack').text(myAttack));
-    
-}
-function drawYourHealth( yourHealth ) {
-    
+function drawYourHealth ( yourHealth ) {
+
+	$('.shown-your-health').remove();
 	$('#your-health-display').append(
-	    $('<span/>').addClass('shown-your-health').text(yourHealth));
-    
+		$('<span/>').addClass('shown-your-health').text(yourHealth));
 }
 function drawYourCounter( yourCounter ) {
-    
+	$('.shown-counter').remove();
 	$('#counter-display').append(
-	    $('<span/>').addClass('shown-counter').text(yourCounter));
-    
+		$('<span/>').addClass('shown-counter').text(yourCounter));
 }
 function drawPicture( myPicture ) {
     
@@ -302,17 +276,6 @@ function pickChallengers(ndx) {
 		
 
 }
-function updateWord( answer ) {
-    $k = $('.shown-letter:first');
-    for ( i in answer ) {
-	if ( answer.charAt(i) != '_' ) {
-	    $k.text( answer.charAt(i) );
-	} else { 
-	    $k.html('&nbsp;');
-	}
-	$k = $k.next();
-    }
-}
 
 // --- END ui ---
 // --- 
@@ -322,18 +285,6 @@ function updateWord( answer ) {
 
 // This array holds the players to choose from.
 // ------Add new words!
-var words = [
-			{name:'Rey Skywalker', hp:169, ap:123},
-			{name:'Jyn Erso', hp:143, ap:123}, 
-			{name:'Finn', hp:196, ap:123}, 
-			{name:'Poe Dameron', hp:143, ap:123}, 
-			{name:'Hans Solo', hp:259, ap:123}, 
-			{name:'Chewbacca', hp:54, ap:123}, 
-			{name:'Storm Trooper', hp:110, ap:123}, 
-			{name:'Top Storm Trooper', hp:243, ap:123}, 
-			{name:'Supreme Leader Snoke', hp:154, ap:123}, 
-			{name:'General Hux', hp:184, ap:123}, 
-			{name:'Darth Vadr', hp:87, ap:123}]
 
 var players = [
 			{pix:' <img id="toggle" onclick="pickChallengers(0)" src="assets/images/reyskywalker2.png" style="width: 48; height: 48; "> ', 
@@ -349,36 +300,7 @@ var players = [
 			{pix:' <img id="toggle" onclick="pickChallengers(5)" src="assets/images/generalhux.png" style="width: 48; height: 48;" > ', 
 			name:'General Hux', hp:184, ap:165} 
 			]
-
-var healthPoints = ['<p style="color: white; font-size: 16px;">Health Points: </p>', 
-					'<p style="color: white; font-size: 16px;">Health Points: </p>', 
-					'<p style="color: white; font-size: 16px;">Health Points: </p>', 
-					'<p style="color: white; font-size: 16px;">Health Points: </p>', 
-					'<p style="color: white; font-size: 16px;">Health Points: </p>', 
-					'<p style="color: white; font-size: 16px;">Health Points: </p>', 
-					'<p style="color: white; font-size: 16px;">Health Points: </p>', 
-					'<p style="color: white; font-size: 16px;">Health Points: </p>', 
-					'<p style="color: white; font-size: 16px;">Health Points: </p>', 
-					'<p style="color: white; font-size: 16px;">Health Points: </p>', 
-					'<p style="color: white; font-size: 16px;">Health Points: </p>'];
-
 			 
-var hints = ['THE MAN WHO PASSES THE SENTENCE SHOULD SWING THE SWORD.',
-			 'THE THINGS I DO FOR LOVE.',
-			 'THE NEXT TIME YOU RAISE A HAND TO ME WILL BE THE LAST TIME YOU HAVE HANDS!',
-			 "IT'S THE FAMILY NAME THAT LIVES ON. IT'S ALL THAT LIVES ON.",
-			 'WHEN YOU PLAY THE GAME OF THRONES, YOU WIN OR YOU DIE.',
-			 'I LEARNED HOW TO DIE A LONG TIME AGO.',
-			 'A DRUNKEN DWARF WILL NEVER BE THE SAVIOR OF THE SEVEN KINGDOMS.',
-			 "I'M NOT GOING TO STOP THE WHEEL. I'M GOING TO BREAK THE WHEEL.",
-			 "A GIRL GIVES A MAN HIS OWN NAME?",
-			 "CHAOS ISN'T A PIT. CHAOS IS A LADDER.",
-			 "I'M GONNA HAVE TO EAT EVERY F--KING CHICKEN IN THIS ROOM.",
-			 'YOU KNOW NOTHING, JON SNOW.',
-			 'THE GOOD LORDS ARE DEAD, AND THE REST ARE MONSTERS.',
-			 'OLLY, BRING ME MY SWORD.',
-			 'HE ALWAYS COMES BACK!']; 
-
 var himgs = [' <img id="toggle" onclick="pickChallengers(0)" src="assets/images/reyskywalker.png" style="width: 48; height: 48; "> ',
 			 ' <img id="toggle" onclick="pickChallengers(1)" src="assets/images/jynerso.png" style="width: 48; height: 48;" > ',
 			 ' <img id="toggle" onclick="pickChallengers(2)" src="assets/images/finn.png" style="width: 48; height: 48;" > ',
@@ -389,46 +311,6 @@ var himgs = [' <img id="toggle" onclick="pickChallengers(0)" src="assets/images/
 			 ' <img id="toggle" onclick="pickChallengers(7)" src="assets/images/TopStormTrooper.jpg" style="width: 48; height: 48;" > ',
 			 ' <img id="toggle" onclick="pickChallengers(8)" src="assets/images/supremeleadersnoke.png" style="width: 48; height: 48;" > ',
 			 ' <img id="toggle" onclick="pickChallengers(9)" src="assets/images/generalhux.png" style="width: 48; height: 48;" > ',
-			 ' <img id="toggle" onclick="pickChallengers(10)" src="assets/images/darthvader.jpg" style="width: 48; height: 48;" > ']; 
-			
-
-			
-function chooseWord () {
-    return words[Math.floor(Math.random() * words.length)];
-}
-function chooseHint (answerHint) {
-	hintValue = hints[answerHint];
-	hintPicture = himgs[answerHint];
-	drawHints(hintValue);
-	drawPicture(hintPicture);
-    console.log("The value of hintValue is: " + hintValue);
-	console.log("The value of answerHint is: " + answerHint);
-}
-
-function blanksFromAnswer ( answerWord ) {  
-    chooseHint(answerWord);
-	var result = ""; 
-    for ( i in answerWord ) {
-        if (answerWord.charAt(i) != " ") { 
-			result = "_" + result;
-		} else {
-			result = " " + result;
-		}
-    }
-	console.log("This is the value of result: " + result);
-    return result;
-}
-function alterAt ( n, c, originalString ) {
-    return originalString.substr(0,n) + c + originalString.substr(n+1,originalString.length);
-}
-function guessLetter( letter, shown, answer ) {
-    var checkIndex = 0;
-    
-    checkIndex = answer.indexOf(letter);
-    while ( checkIndex >= 0 ) {
-        shown = alterAt( checkIndex, letter, shown );
-        checkIndex = answer.indexOf(letter, checkIndex + 1);
-    }
-    return shown;
-}
-
+			 ' <img id="toggle" onclick="pickChallengers(10)" src="assets/images/darthvader.jpg" style="width: 48; height: 48;" > '
+			 ]; 
+// --- End of Program --- 			
